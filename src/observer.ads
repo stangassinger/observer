f40af3a04deffd9dd0aedab7f906c290bdbdbacc
stangassinger <guenther.stangassinger@gmx.de>
2018-05-20 07:11:47 +0000

    abstract type

--- src/observer.ads
+++ src/observer.ads
@@ -2,9 +2,12 @@ with subject;
 
 package observer is
 
-   type obj is abstract tagged null record;
+   type obj is abstract tagged limited private;
 
    procedure update (Self : in obj) is abstract;
+
+private
+   type obj is abstract tagged limited null record;
 
 
 end observer;
